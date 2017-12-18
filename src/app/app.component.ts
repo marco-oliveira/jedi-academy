2d01cb04da00f90eb2620cbefccfe4497bbf6eca
marco-oliveira <marco-oliveira@live.com>
2017-12-18 16:11:03 -0200

    Utilizando diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Student} from "./student/student.model";
 
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'coruscant'},
+    {name: 'Han Solo', isJedi: false},
+    {name: 'Leia', isJedi: false}
+  ]
+
 }
